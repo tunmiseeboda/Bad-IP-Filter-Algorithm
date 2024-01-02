a20e3b2ed81739c5e4b48c9ec8e9ab00ca419b78
Tunmise Eboda <138678066+tunmiseeboda@users.noreply.github.com>
2024-01-02 15:08:07 -0500

    Update IP_Filter.py

--- IP_Filter.py
+++ IP_Filter.py
@@ -1,6 +1,5 @@
 #Programmer name: Tunmi:se Eboda
 #Program date: december 9th, 2022
-#My twenty-third tbh i lost count program 
 #Final coding project
 
 import re
@@ -23,9 +22,8 @@ def input_file(input_filename):
             if test1 or test2 or test3:
                 compromised_ip.append(ip)
                 compromised_ip_dates.append(dates)
-    except:
-        print()
-        print(f'Error-- There is an issue with file {input_filename}. Please reenter: ')
+    except FileNotFoundError:
+        print(f'\nError-- There is an issue with file {input_filename}. Please reenter: ')
     finally:
         if input1:
             input1.close()  # Close the file in the finally block
@@ -36,9 +34,8 @@ def output_file(output_filename):
     try:
         output = open(output_filename, "w")
         return output
-    except:
-        print()
-        print(f'Error-- There is an issue with file {output_filename}. Please reenter: ')
+    except IOError:
+        print(f'\nError-- There is an issue with file {output_filename}. Please reenter: ')
 
 
 
@@ -58,62 +55,21 @@ while True:
     output_ = output_file(output_filename)
 
     if(output_):
-        print()
-        print('Output Report')
+        print('\nOutput Report')
         print('-------------')
         print('The total number of records in the file is: ',total_ips)
-        print()
-        print('The number of suspect IP addresses is: ', suspect_ip)
-        print()
-        print('The percentage of suspect IP addresses is: %.4f' % percentage)
-        print()
-        print('Suspect IP Addresses')
-        print('--------------------')
-        print()
-        print('Ip Address = ', input_[0][0], ' Date and Time = ',input_[2][0])
-        print()
-        print('Ip Address = ', input_[0][1], ' Date and Time = ',input_[2][1])
-        print()
-        print('Ip Address = ', input_[0][2], ' Date and Time = ',input_[2][2])
-        print()
-        print('Ip Address = ', input_[0][3], ' Date and Time = ',input_[2][3])
-        print()
-        print('Ip Address = ', input_[0][4], ' Date and Time = ',input_[2][4])
-        print()
-        print('Ip Address = ', input_[0][5], ' Date and Time = ',input_[2][5])
-        print()
-        print('Ip Address = ', input_[0][6], ' Date and Time = ',input_[2][6])
-        print()
-        print('Ip Address = ', input_[0][7], ' Date and Time = ',input_[2][7])
-        print()
-        print('Ip Address = ', input_[0][8], ' Date and Time = ',input_[2][8])
-        print()
-        print('Ip Address = ', input_[0][9], ' Date and Time = ',input_[2][9])
-        print()
-        print('Ip Address = ', input_[0][10], ' Date and Time = ',input_[2][10])
-        print()
-        print('Ip Address = ', input_[0][11], ' Date and Time = ',input_[2][11])
-        print()
-        print('Ip Address = ', input_[0][12], ' Date and Time = ',input_[2][12])
-        print()
-        print('Ip Address = ', input_[0][13], ' Date and Time = ',input_[2][13])
-        print()
-        print('Ip Address = ', input_[0][14], ' Date and Time = ',input_[2][14])
-        print()
-        print('Ip Address = ', input_[0][15], ' Date and Time = ',input_[2][15])
-        print()
-        print('Ip Address = ', input_[0][16], ' Date and Time = ',input_[2][16])
-        print()
-        print('Ip Address = ', input_[0][17], ' Date and Time = ',input_[2][17])
-        print()
-        print('Ip Address = ', input_[0][18], ' Date and Time = ',input_[2][18])
-        print()
+        print('\nThe number of suspect IP addresses is: ', suspect_ip)
+        print('\nThe percentage of suspect IP addresses is: %.4f' % percentage)
+        print('\nSuspect IP Addresses')
+        print('--------------------\n')
         
-        
-        break
-    else:
-        continue
+        for i in range(min(len(input_[0]), 19)):
+              date_string = " ".join(input_[2][i])  # Remove brackets and newline characters
+              output_.write("Ip Address = " + input_[0][i] + "   Date and Time = " + date_string + "\n")
 
-output_.close
-print()
+        output_.close()  # Close the output file
+        break
+
+with open(output_filename, "r") as output:
+    print(output.read())
 print('program complete')
